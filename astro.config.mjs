31c196956c88e64bd08fbb090a0183bac0064b9d
simplearyan <aryanphone00620@gmail.com>
2025-12-02 08:28:56 +0530

    Made CivicThesis Installable (PWA) 📌 🍃

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +14,33 @@ export default defineConfig({
   integrations: [mdx({
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
-  }), sitemap()]
+  }), sitemap()],
+  vite: {
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'CivicThesis',
+          short_name: 'CivicThesis',
+          description: 'A minimal, modern blog.',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        },
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        }
+      })
+    ]
+  }
 });
